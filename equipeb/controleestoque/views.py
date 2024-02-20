a7a20c0c08d69649242edc65fce1bba1152916bf
Anna Victoria Feliciana <anna.feliciana@upe.br>
2024-02-20 08:51:34 -0300

    tabela user mostrando no front

--- equipeb/controleestoque/views.py
+++ equipeb/controleestoque/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User  
 
 # Create your views here.
 
 def index(request):
+    
+    user = User.objects.all()
+    
     title = 'Página Inicial, bem vindo(a)!'
     context = {
 	    "title": title,
+        'users': user,
 	}
-    return render(request, "index.html",context)
+    return render(request, "index.html",context) 
