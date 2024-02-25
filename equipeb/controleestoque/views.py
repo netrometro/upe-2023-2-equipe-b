b788c7759b65823343c8c35093ef9573b0ff144f
Anna Victoria Feliciana <anna.feliciana@upe.br>
2024-02-25 20:52:45 -0300

    back do form

--- equipeb/controleestoque/views.py
+++ equipeb/controleestoque/views.py
@@ -1,73 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User 
 from .models import Produtos
 from .forms import ProdutosFormCriar
 from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
-
 def index(request):
-    
     user = User.objects.all()
     produtos = Produtos.objects.all()
-    
     title = 'Página Inicial, bem vindo(a)!'
     context = {
-	    "title": title,
+        "title": title,
         'users': user,
-	}
-    return render(request, "index.html",context) 
+    }
+    return render(request, "index.html", context) 
 
-def listar_item(request):
-	title = 'Lista de Itens'
-	produtos = Produtos.objects.all()
-	context = {
-		"title": title,
-		"produtos": produtos,
-	}
-	return render(request, "listar_item.html", context)
+def listar_produto(request):
+    title = 'Lista de Itens'
+    produtos = Produtos.objects.all()
+    context = {
+        "title": title,
+        "produtos": produtos,
+    }
+    return render(request, "listar_produto.html", context)
 
-def adicionar_produto(request):
-	form = ProdutosFormCriar(request.POST or None)
-	if form.is_valid():
-		form.save()
-	context = {
-		"form": form,
-		"title": "Adicionar um produto",
-	}
-	return render(request, "listar_item.html", context)
-
-def listar_item(request):
-	title = 'Lista de Itens'
-	produtos = Produtos.objects.all()
-	context = {
-		"title": title,
-		"produtos": produtos,
-	}
-	return render(request, "listar_item.html", context)
 
 def adicionar_produto(request):
-	form = ProdutosFormCriar(request.POST or None)
-	if form.is_valid():
-		form.save()
-	context = {
-		"form": form,
-		"title": "Adicionar um produto",
-	}
-	return render(request, "listar_item.html", context)
-
+    form = ProdutosFormCriar(request.POST or None)
+    if form.is_valid():
+        form.save()
+        # redirect to a success URL listar_produto
+        return redirect('listar_produto')
+    context = {
+        "form": form,
+    }
+    return render(request, "adicionar_produto.html", context)
 
 
 def register(request):  
-    if request.POST == 'POST':  
-        form = UserCreationForm()  
+    if request.method == 'POST':  
+        form = UserCreationForm(request.POST)  
         if form.is_valid():  
             form.save()  
-        messages.success(request, 'Account created successfully')  
-  
+            return render(request, 'register_success.html')  
     else:  
         form = UserCreationForm()  
     context = {  
-        'form':form  
+        'form': form  
     }  
-    return render(request, 'register.html', context)  
+    return render(request, 'register.html', context)
