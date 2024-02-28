f814d01ab5ccffd6efbfc0e70ae67a951fefc793
PedroFerreiraLopes <jpedro.serrita@gmail.com>
2024-02-28 11:43:48 -0300

    Path de deleção de fornecedor

--- equipeb/controleestoque/urls.py
+++ equipeb/controleestoque/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from controleestoque.views import index, listar_produto, adicionar_produto, list_supplier, update_supplier, new_supplier
+from controleestoque.views import index, listar_produto, adicionar_produto, list_supplier, update_supplier, new_supplier, delete_supplier
 
 urlpatterns = [
     path("", index, name="index"),
@@ -9,4 +9,5 @@ urlpatterns = [
     path("list_supplier", list_supplier, name="list_supplier"),
     path("update_supplier/<id>", update_supplier, name="update_supplier"),
     path("new_supplier", new_supplier, name="new_supplier"),
+    path("delete_supplier/<id>", delete_supplier, name="delete_supplier"),
 ]
