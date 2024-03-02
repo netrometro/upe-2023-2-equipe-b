aba8ecde419a66d64e4414f1a844768bcdfb5974
Anna Victoria Feliciana <anna.feliciana@upe.br>
2024-03-02 14:04:30 -0300

    gerando o gráfico

--- equipeb/controleestoque/urls.py
+++ equipeb/controleestoque/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from controleestoque.views import index, listar_produto, adicionar_produto, list_supplier, update_supplier, new_supplier
 from . import views
 from .views import apagar_produto
+from .views import line_chart, line_chart_json
 
 urlpatterns = [
     path("", index, name="index"),
@@ -13,4 +14,7 @@ urlpatterns = [
     path("list_supplier", list_supplier, name="list_supplier"),
     path("update_supplier/<id>", update_supplier, name="update_supplier"),
     path("new_supplier", new_supplier, name="new_supplier"),
+    path('bar_produtos_chart', views.bar_produtos_chart, name='bar_produtos_chart'),
+    path('chart', line_chart, name='line_chart'),
+    path('chartJSON', line_chart_json, name='line_chart_json'),
 ]
--- equipeb/controleestoque/views.py
+++ equipeb/controleestoque/views.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from .models import Produtos, Fornecedor
 from .forms import ProdutosFormCriar, FornecedorForm
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView
+from chartjs.views.lines import BaseLineChartView
+from django.shortcuts import render
+from django.db.models import Sum
+from django.http import JsonResponse
 
 def index(request):
     # Obtém todos os usuários
@@ -129,3 +134,37 @@ def register(request):
         'form': form  
     }  
     return render(request, 'register.html', context)
+
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return 7 labels for the x-axis."""
+        return ["January", "February", "March", "April", "May", "June", "July"]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Central", "Eastside", "Westside"]
+
+    def get_data(self):
+        """Return 3 datasets to plot."""
+
+        return [[75, 44, 92, 11, 44, 95, 35],
+                [41, 92, 18, 3, 73, 87, 92],
+                [87, 21, 94, 3, 90, 13, 65]]
+
+
+line_chart = TemplateView.as_view(template_name='line_chart.html')
+line_chart_json = LineChartJSONView.as_view()
+
+def bar_produtos_chart(request):
+    labels = []
+    data = []
+
+    queryset = Produtos.objects.values('nome').annotate(quantidade=Sum('quantidade'))
+    for produto in queryset:
+        labels.append(produto['nome'])  # Alteração aqui: corrigir a atribuição do nome do produto
+        data.append(produto['quantidade'])
+
+    return JsonResponse({
+        'labels': labels,
+        'data': data,
+    })
